4ba1eeff79735ae9ebb2bc5e3d1535ec6fa7ab9b
kyachingprue <kyachingpruemarma.info@gmail.com>
2025-04-08 22:52:26 +0600

    new commit

--- index.js
+++ index.js
@@ -30,13 +30,19 @@ async function run() {
     );
 
     const jobCollection = client.db('jobManagement').collection('jobs');
+
     const jobApplicationCollection = client
       .db('jobManagement')
       .collection('job_applications');
 
     //Job related APIs
     app.get('/jobs', async (req, res) => {
-      const cursor = jobCollection.find();
+      const email = req.query.email;
+      let query = {};
+      if (email) {
+        query = { hr_email: email };
+      }
+      const cursor = jobCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -45,6 +51,12 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await jobCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post('/jobs', async (req, res) => {
+      const newJob = req.body;
+      const result = await jobCollection.insertOne(newJob);
       res.send(result);
     });
 
@@ -69,9 +81,32 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/job-applications/jobs/:job_id', async (req, res) => {
+      const jobId = req.params.job_id;
+      const query = { job_id: jobId };
+      const result = await jobApplicationCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post('/job-applications', async (req, res) => {
       const application = req.body;
       const result = await jobApplicationCollection.insertOne(application);
+      res.send(result);
+    });
+
+    app.patch('/job-applications/:id', async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: data.status,
+        },
+      };
+      const result = await jobApplicationCollection.updateOne(
+        filter,
+        updatedDoc
+      );
       res.send(result);
     });
   } finally {
